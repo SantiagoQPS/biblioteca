6a52a66017960986471cc16cfb7ad6c3023d030e
SantiagoQPS <santiago.quintero@correounivalle.edu.co>
2019-08-08 10:45:12 -0500

    video 7

--- apps/libro/models.py
+++ apps/libro/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField('Nombre', max_length = 200, blank = False, null = False)
+    apellidos = models.CharField('Apellido', max_length = 200, blank = False, null = False)
+    nacionalidad = models.CharField('Nacionalidad', max_length = 200, blank = False, null = False)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+        ordering = ['nombre'] #ordena alfabeticamente por nombre
+
+    def __str__(self):
+        return self.nombre
