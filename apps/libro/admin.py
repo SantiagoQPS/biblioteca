8adf97a6893ea1c48a0da6d3e915ef86c2466d6b
SantiagoQPS <santiago.quintero@correounivalle.edu.co>
2019-08-15 09:07:00 -0500

    video-11

--- apps/libro/admin.py
+++ apps/libro/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Autor
+from .models import Libro
 
 admin.site.register(Autor)
+admin.site.register(Libro)
--- apps/libro/models.py
+++ apps/libro/models.py
@@ -5,6 +5,7 @@ class Autor(models.Model):
     nombre = models.CharField('Nombre', max_length = 200, blank = False, null = False)
     apellidos = models.CharField('Apellido', max_length = 200, blank = False, null = False)
     nacionalidad = models.CharField('Nacionalidad', max_length = 200, blank = False, null = False)
+    fecha_creacion = models.DateField('Fecha de creacion', auto_now=True, auto_now_add=False)
 
     class Meta:
         verbose_name = 'Autor'
@@ -13,3 +14,20 @@ class Autor(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+class Libro(models.Model):
+    id = models.AutoField(primary_key = True)
+    titulo = models.CharField('Titulo', max_length = 225, blank = False, null = False)
+    fecha_publicacion = models.DateField('Fecha de publicacion', blank = False, null = False)
+    autor_id = models.ManyToManyField(Autor)
+    fecha_creacion = models.DateField('Fecha de creacion', auto_now=True, auto_now_add=False)
+
+    class Meta:
+        verbose_name = 'Libro'
+        verbose_name_plural = 'Libros'
+        ordering = ['titulo']
+
+    def __str__(self):
+        return self.titulo
+        
